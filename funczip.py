262c40417351472d8a88946eb73fe14c615a41e1
not logged in or am I? <94306853+thatrandomperson5@users.noreply.github.com>
2022-08-19 16:46:40 -0500

    Update funczip.py

--- funczip.py
+++ funczip.py
@@ -1,14 +1,15 @@
+from typing import Callable, Any
 class funczip:
-    def __init__(self, func, *args, **kwargs):
+    def __init__(self, func: Callable, *args: Any, **kwargs: Any):
         self.func = func
         self.args = args
         self.kwargs = kwargs
 
-    def __str__(self):
+    def __str__(self) -> str:
         a = [f"{x}" for x in self.args]
 
         k = [f"{x}={y}" for x, y in self.kwargs.items()]
         return f"{self.func.__name__}({', '.join(a+k)})"
 
-    def __call__(self):
+    def __call__(self) -> Any:
         return self.func(*self.args, **self.kwargs)
