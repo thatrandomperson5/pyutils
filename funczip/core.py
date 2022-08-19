062ba9e4a1a39798410963c62b5e5612522a2613
not logged in or am I? <94306853+thatrandomperson5@users.noreply.github.com>
2022-08-19 18:36:37 -0500

    Update core.py

--- funczip/core.py
+++ funczip/core.py
@@ -1,11 +1,12 @@
-from typing import Callable, Any
+from typing import Callable, Any, List
 
 
 class FuncZip:
     def __init__(self, func: Callable, *args: Any, **kwargs: Any):
-        self.func = func
-        self.args = args
-        self.kwargs = kwargs
+        self.func: Callable = func
+        self.args: Any = args
+        self.kwargs: Any = kwargs
+        self.modifiers:List[Callable]  = []
 
     def __str__(self) -> str:
         a = [f"{x}" for x in self.args]
@@ -18,7 +19,8 @@ class FuncZip:
 
         k = [f"{x}={y}" for x, y in self.kwargs.items()]
         return f"<funczip target={self.func.__name__} arguments={', '.join(a+k)}>"
-
+    def add_mod(self, mod: Callable):
+        self.modifiers.append(mod)
     def __call__(self) -> Any:
         return self.func(*self.args, **self.kwargs)
 
