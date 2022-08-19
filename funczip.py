b30bea78f4e58d50ba6e801ff1193164d1daa1e8
not logged in or am I? <94306853+thatrandomperson5@users.noreply.github.com>
2022-08-19 16:31:56 -0500

    Create funczip.py

---
+++ funczip.py
@@ -0,0 +1,13 @@
+class funczip:
+    def __init__(self, func, *args, **kwargs):
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+    def __str__(self):
+        a = [f"{x}" for x in self.args]
+        
+        k = [f"{x}={y}" for x, y in self.kwargs.items()]
+        return f"{self.func.__name__}({', '.join(a+k)})"
+    def __call__(self):
+        return self.func(*self.args, **self.kwargs)
+        
