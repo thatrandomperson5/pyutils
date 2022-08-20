6e6ba9bf767fb5a64dd96786c8b8607a0ec060e8
not logged in or am I? <94306853+thatrandomperson5@users.noreply.github.com>
2022-08-19 19:57:22 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -11,10 +11,21 @@ z = funczip.FuncZip(foo, "hello world", a="9", b=10)
 print(z)
 z()
 
+class Remote:
+    def __init__(self,*args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+    def add(self, func):
+        func.args += self.args
+        func.kwargs.update(self.kwargs)
+        return func
 
+r = Remote("a", "b")
+
+@funczip.mod(r.add)
 @funczip.zip("Hello world")
-def bar(val):
-    print(val)
+def bar(val, a, b):
+    print(val, a, b)
 
 
 bar()
