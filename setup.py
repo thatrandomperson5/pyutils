9009af1b0314479a79495b11db066a79bbcc0939
not logged in or am I? <94306853+thatrandomperson5@users.noreply.github.com>
2022-08-23 09:30:32 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from distutils.core import setup
 
+pkgs=[]
+if "--funczip" in sys.argv:
+      pkgs = ['funczip', 'funczip.stack']
+
+
 setup(name='funczip',
       version='0.1.0',
       description='A 100% useless python function zipping package.',
       author='ThatRandomPerson5',
-      packages=['funczip', 'funczip.stack'],
+      packages=pkgs,
      )
