72940dc902b951fc2a70925a04a459a485138fc0
thatrandomperson5 <thatrandomperson5@users.noreply.github.com>
2022-08-23 14:44:23 +0000

    :art: Format Python code with psf/black

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools.command.install import install
 from distutils.core import setup
 
+
 class InstallCommand(install):
     user_options = install.user_options + [
-        ('funczip', None, None), # a 'flag' option
+        ("funczip", None, None),  # a 'flag' option
     ]
 
     def initialize_options(self):
         install.initialize_options(self)
         self.funczip = None
-        
 
     def finalize_options(self):
-        
+
         install.finalize_options(self)
 
     def run(self):
         global funczip
-        funczip = self.funczip # will be 1 or None
+        funczip = self.funczip  # will be 1 or None
         install.run(self)
 
 
@@ -27,6 +27,6 @@ setup(
     description="A 100% useless python function zipping package.",
     author="ThatRandomPerson5",
     cmdclass={
-        'install': InstallCommand,
+        "install": InstallCommand,
     },
 )
