ea8c99462128c336f595920e1e2bc30f017541d7
not logged in or am I? <94306853+thatrandomperson5@users.noreply.github.com>
2022-08-23 09:17:17 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='funczip',
+      version='0.1.0',
+      description='A 100% useless python function zipping package.',
+      author='ThatRandomPerson5',
+      packages=['funczip'],
+     )
