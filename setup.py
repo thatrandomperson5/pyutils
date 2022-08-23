5ce4ccd10445e7a23c6b477c9bf39a53ff88a78d
thatrandomperson5 <thatrandomperson5@users.noreply.github.com>
2022-08-23 14:21:07 +0000

    :art: Format Python code with psf/black

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup
 
-setup(name='funczip',
-      version='0.1.0',
-      description='A 100% useless python function zipping package.',
-      author='ThatRandomPerson5',
-      packages=['funczip', 'funczip.stack'],
-     )
+setup(
+    name="funczip",
+    version="0.1.0",
+    description="A 100% useless python function zipping package.",
+    author="ThatRandomPerson5",
+    packages=["funczip", "funczip.stack"],
+)
