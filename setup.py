2530192c541dabc2a93f7a343cbeb86b17d4a756
not logged in or am I? <94306853+thatrandomperson5@users.noreply.github.com>
2022-08-23 13:32:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,16 @@ from distutils.core import setup
 import sys
 
 
-global args
-args = {}
-copy_args = sys.argv[1:]
-if "funczip" in copy_args:
-    copy_args.remove("funczip")
+
+if 1:
+    
     args = dict(
         name="funczip",
         version="0.1.1",
         description="A 100% useless python function zipping package.",
         packages=["funczip", "funczip.stack"],
     )
-print(args)
+
 setup(
     script_args=copy_args,
     author="ThatRandomPerson5",
