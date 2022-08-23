2dae48a914f310ad2a8c76a049b986acef64394c
not logged in or am I? <94306853+thatrandomperson5@users.noreply.github.com>
2022-08-23 09:20:45 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(name='funczip',
       version='0.1.0',
       description='A 100% useless python function zipping package.',
       author='ThatRandomPerson5',
-      packages=['funczip'],
+      packages=['funczip', 'funczip.stack'],
      )
