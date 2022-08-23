480ee7654f564b5d263bfa5b869f45ba62114250
thatrandomperson5 <thatrandomperson5@users.noreply.github.com>
2022-08-23 14:30:59 +0000

    :art: Format Python code with psf/black

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
 
-pkgs=[]
+pkgs = []
 if "--funczip" in sys.argv:
-      pkgs = ['funczip', 'funczip.stack']
+    pkgs = ["funczip", "funczip.stack"]
 
 
-setup(name='funczip',
-      version='0.1.0',
-      description='A 100% useless python function zipping package.',
-      author='ThatRandomPerson5',
-      packages=pkgs,
-     )
+setup(
+    name="funczip",
+    version="0.1.0",
+    description="A 100% useless python function zipping package.",
+    author="ThatRandomPerson5",
+    packages=pkgs,
+)
