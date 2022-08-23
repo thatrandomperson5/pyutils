ee909bbdbbe5571eb22531f2c2467921e17a3632
not logged in or am I? <94306853+thatrandomperson5@users.noreply.github.com>
2022-08-23 09:43:42 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
+from setuptools.command.install import install
 from distutils.core import setup
 
-pkgs = []
-if "--funczip" in sys.argv:
-    pkgs = ["funczip", "funczip.stack"]
+class InstallCommand(install):
+    user_options = install.user_options + [
+        ('funczip', None, None), # a 'flag' option
+    ]
+
+    def initialize_options(self):
+        install.initialize_options(self)
+        self.funczip = None
+        
+
+    def finalize_options(self):
+        
+        install.finalize_options(self)
+
+    def run(self):
+        global funczip
+        funczip = self.funczip # will be 1 or None
+        install.run(self)
 
 
 setup(
@@ -10,5 +26,7 @@ setup(
     version="0.1.0",
     description="A 100% useless python function zipping package.",
     author="ThatRandomPerson5",
-    packages=pkgs,
+    cmdclass={
+        'install': InstallCommand,
+    },
 )
