bbe2201c7b648869415a6172e785c1b7a13cf579
not logged in or am I? <94306853+thatrandomperson5@users.noreply.github.com>
2022-08-26 21:13:46 -0500

    Update functrain.py

--- functrain.py
+++ functrain.py
@@ -1,1 +1,17 @@
+from dataclasses import dataclass
+from typing import Any
 
+@dataclass
+class Trainable:
+    value: Any
+    def next(self, func, *args, **kwargs):
+        a = func(self.value, *args, **kwargs)
+        self.value = a
+        
+        return self
+    def __call__(self):
+        return self.end()
+    def end(self):
+        return self.value
+        
+        
